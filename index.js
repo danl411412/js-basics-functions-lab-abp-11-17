c6ed6e37b1fb43e43a9f3e2d8637e9a17d03a565
danl411412 <daan.b.lee@gmail.com>
2017-12-19 15:46:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ function distanceFromHqInFeet(num) {
 function distanceTravelledInFeet(from, to) {
   return Math.abs(from - to) * 264;
 }
+
+function calculatesFarePrice(from, to) {
+  var distanceInFeet = distanceTravelledInFeet(from, to);
+  if(distanceInFeet <= 400) {
+    return 0;
+  } else if(distanceInFeet > 400 && distanceInFeet <= 2000) {
+    return distanceInFeet * .02;
+  } else if(distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return 25;
+  } else{
+    return "cannot travel that far";
+  }
+}
